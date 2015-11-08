2af27c10701c592c887542ba32f6609f5f832d60
dtex <donovan@donovan.bz>
2015-11-08 08:36:39 -0600

    Remove references to joint names

--- index.js
+++ index.js
@@ -327,57 +327,65 @@ var findValidAngle = function(angle, range) {
 var ikSolvers = {
   "ZYY": function(chain, offsetPosition) {
 
+    var angles = [], degrees = [];
+
     // Put the coordinates into seperate variables for readability
     var xd = offsetPosition[0];
     var yd = offsetPosition[1];
     var zd = offsetPosition[2];
+
+    // Calculate the first joint angle in radians.
+    // This is a simple 2D right triangle solve (yay)
+    angles[0] = Math.atan(yd/xd);
+
+    // Make sure we have a the correct solution
+    degrees[0] = findValidAngle(angles[0], chain.devices[0].range);
+
+    // We still need this in radians
+    angles[0] = degToRad(degrees[0]);
+
+    // Subtract the first segment from our offset
+    yd = yd - Math.cos(angles[0]) * chain.segments.coxa;
+    xd = xd - Math.sin(angles[0]) * chain.segments.coxa;
 
     // We use the squares of these a lot so let's store the result
     var xd_sq = xd * xd;
     var yd_sq = yd * yd;
     var zd_sq = zd * zd;
 
-    // This is the 3D hypoteneuse from the origin point to the end effector
+    // This is the 3D hypoteneuse from the 2nd joint to the end effector
     var hypot = Math.sqrt(xd_sq + yd_sq + zd_sq);
 
     // This is the 2D hypoteneuse on the x/y plane.
     var hypot2d = Math.sqrt(xd_sq + yd_sq);
 
-    // Calculate the coxa angle in radians.
-    // This is a simple 2D right triangle solve (yay)
-    var coxaAngle = Math.atan(yd/xd);
-
     // This is a slightly tougher triangle solve
-    var tibiaAngle = solveAngle(chain.segments.femur, chain.segments.tibia, hypot);
+    angles[2] = solveAngle(chain.segments.femur, chain.segments.tibia, hypot);
 
     // Our last triangle solve
-    var femurAngle = solveAngle(chain.segments.femur, hypot, chain.segments.tibia);
+    angles[1] = solveAngle(chain.segments.femur, hypot, chain.segments.tibia);
 
-    // But wait! The femur angle returned represents the angle between the
-    // femur and the 2D hypoteneuse but we actually want the angle between
-    // the femur servo's axis of rotation and the femur so we need to
-    // subtract the angle described by the end effector, the femur origin
-    // and the femur's axis of rotation.
+    // But wait! The 2nd joint angle returned represents the angle between the
+    // 2nd joint and the 2D hypoteneuse but we actually want the angle between
+    // the 2nd joint servo's axis of rotation and the 2nd joint so we need to
+    // subtract the angle described by the end effector, the 2nd joint origin
+    // and the 2nd joint's axis of rotation.
     // It really is easier than I make it sound.
-    femurAngle += Math.sin(zd/hypot2d);
+    angles[1] += Math.sin(zd/hypot2d);
 
     // If the chain is on the left side we need to modify our solutions
     if (offsetPosition[0] < 0) {
-      femurAngle = Math.PI - femurAngle;
-      tibiaAngle = Math.PI - tibiaAngle;
+      angles[1] = Math.PI - angles[1];
+      angles[2] = Math.PI - angles[2];
     }
 
     // Just two things left: The angles are in radians (we need degrees
     // for our servo) and the angles may or may not be within our servo's
     // range. Fix that with findValidAngle().
-    var coxaDegrees = findValidAngle(coxaAngle, chain.devices[0].range);
-    var femurDegrees = findValidAngle(femurAngle, chain.devices[1].range);
-    var tibiaDegrees = findValidAngle(tibiaAngle, chain.devices[2].range);
+    degrees[1] = findValidAngle(angles[1], chain.devices[1].range);
+    degrees[2] = findValidAngle(angles[2], chain.devices[2].range);
 
-    // Our solution Array
-    var angles = [coxaDegrees, femurDegrees, tibiaDegrees];
-
-    return angles;
+    return degrees;
   }
 };
 
